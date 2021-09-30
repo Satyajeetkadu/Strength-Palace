bce12b7cfb044d6d0a924f0fdafd357d3a541a03
Satyajeetkadu <satyajeetkadu74@gmail.com>
2021-09-30 12:02:44 +0530

    first commit

---
+++ js/contact.js
@@ -0,0 +1,29 @@
+var config = {
+  apiKey: "AIzaSyDbeZtbOqSg_brZQskWeadJ-VLwdAxwN-c",
+  authDomain: "gymwebsite-c4fd8.firebaseapp.com",
+  databaseURL: "https://gymwebsite-c4fd8-default-rtdb.firebaseio.com",
+  projectId: "gymwebsite-c4fd8",
+  storageBucket: "gymwebsite-c4fd8.appspot.com",
+  messagingSenderId: "264765439499",
+  appId: "1:264765439499:web:971d6db3aad880c710510e"
+};
+firebase.initializeApp(config);
+
+var messagesRef = firebase.database().ref('contactformmessages');
+
+$('#contactForm').submit(function(e) {
+  e.preventDefault();
+
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+      name: $('.name').val(),
+      email: $('.email').val(),
+      message: $('.message').val()
+  });
+
+  $('.success-message').show();
+
+  $('#contactForm')[0].reset();
+
+  alert('Message sent! We will contact you in 24 hours')
+});
